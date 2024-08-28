633b7740343fdad05fd3abfc5ab80a0c7784abc2
Jorge Vergara <jvmarquez723@gmail.com>
2024-08-28 09:22:18 -0500

    Commit inicial

---
+++ Hello_function.py
@@ -0,0 +1,7 @@
+user_name = input("Ingrese su nombre: ")
+
+def say_hi():
+    print(f"Hola {user_name}")
+
+
+say_hi()
---
+++ min.py
@@ -0,0 +1,8 @@
+def id_menor(num1, num2):
+    if num1 < num2:
+        return num1
+    elif num2 < num1:
+        return num2
+
+resultado = id_menor(100 , 50)
+print(resultado)
